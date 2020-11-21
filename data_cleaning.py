c5abfd400cfab9fe4ff5cf608397d0467b69b3fb
nelsonhwu <69525077+nelsonhwu@users.noreply.github.com>
2020-11-20 17:07:20 -0800

    started cleaning the data

---
+++ data_cleaning.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 20 11:34:06 2020
+
+@author: Nelson J Hwu
+"""
+
+import pandas as pd
+import datetime as dt
+import re
+
+og_data = pd.read_csv('StockX-Data-Contest-2019-3.csv')
+df = og_data.copy()
+
+df.columns
+df.shape
+df.dtypes
+df.describe()
+
+# Checking for null values
+nulls = pd.concat([df.isnull().sum()], axis=1)
+nulls[nulls.sum(axis=1) > 0]
+
+#Change 'order date' dtype
+df['Order Date'] = pd.to_datetime(df['Order Date'], format= '%m/%d/%y').dt.date
+df.head()
+
+# Change 'release date' dtype
+df['Release Date'] = pd.to_datetime(df['Release Date'], format = '%m/%d/%y').dt.date
+df.head()
+
+# Remove $ and comma from sale price
+df['Sale Price'] = df['Sale Price'].apply(lambda x: x.replace('$', '').replace(',', ''))
+
+# Remove $ and comma from retail price
+df['Retail Price'] = df['Retail Price'].apply(lambda x: x.replace('$', '').replace(',', ''))
+
+# Convert price object columns into numerical columns
+obj_cols = ['Sale Price', 'Retail Price']
+for col in obj_cols:
+    df[str(col)] = pd.to_numeric(df[str(col)])
+
+# Remove '-' from sneaker name
+df['Sneaker Name'] = df['Sneaker Name'].apply(lambda x: x.replace('-', ' '))
+
+# Isolate Colors
+shoe_name = df['Sneaker Name']
+
+colors = re.findall( r'Black|White|Red|Orange|Yellow|Green|Blue|Purple', pd.Series.to_string(shoe_name))
+print(pd.Series.to_string(shoe_name))
+print(shoe_name)
+print(colors)
